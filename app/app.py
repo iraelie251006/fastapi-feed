7085e45fe33c3a6b5e98ffce2a735a989ed196c7
Elie <niyubwayoiraelie5777@gmail.com>
2025-11-14 20:50:07 +0200

    Refactor upload_file endpoint to return post details and update created_at default value in Post model

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Form, UploadFile, Depends, File
 from .db import Post, create_db_and_tables, get_async_session
 from contextlib import asynccontextmanager
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,7 +13,7 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/upload")
 async def upload_file(
-    file: UploadFile = File(...),
+    file: UploadFile = File(),
     caption: str = Form(""),
     session: AsyncSession = Depends(get_async_session)
 ):
@@ -26,3 +27,13 @@ async def upload_file(
     session.add(post)
     await session.commit()
     await session.refresh(post)
+    return {
+        "id": str(post.id),
+        "caption": post.caption,
+        "url": post.url,
+        "file_type": post.file_type,
+        "file_name": post.file_name,
+        "created_at": post.created_at.isoformat()
+    }
+
+
--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Text, String, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
-import datetime
+from datetime import datetime
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
@@ -20,7 +20,7 @@ class Post(Base):
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.datetime)
+    created_at = Column(DateTime, default=datetime.now)
 
 
 engine = create_async_engine(
